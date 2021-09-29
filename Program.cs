14dc039b21e8af0726ca3c0b7aa0d16445d4b6e7
Putseyeu <putseyeu.a.d@gmail.com>
2021-09-29 21:37:01 +0200

    step1

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imageHomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalImage = 52;
+            int imageOneRow = 3;
+            int totalRow = totalImage / imageOneRow;
+            int excessImage = totalImage - (totalRow * 3);
+
+            Console.WriteLine (" completely filled rows  " + totalRow + 
+                " pictures will be beyond measure " + excessImage);
+        }
+    }
+}
